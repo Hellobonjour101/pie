dcefda8aa6e975e4c58b0fc278db8e0aae03841a
Hellobonjour101 <43777600+Hellobonjour101@users.noreply.github.com>
2020-04-24 17:30:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const contact = document.querySelector('#contact');
 contact.innerHTML = `
   <div class="container-fluid">
     <a class="instagram" href="#"><i class="social-icon fab fa-instagram fa-2x"></i></a>
-    <a class="mail" href="mailto:hellobonjour.kim@gmail.com"><i class="social-icon fas fa-envelope fa-2x"></i></a>
+    <a class="mail" href="mailto:painahh@naver.com"><i class="social-icon fas fa-envelope fa-2x"></i></a>
     <p>© Copyright 2020 bigTree & Elepant</p>
   </div>
   `;
